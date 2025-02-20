dd757fa6e609338b82db27b1f91d7a93e5f7b53c
YashPratapSingh25 <yash64572@gmail.com>
2025-02-20 11:56:06 +0530

    Create a new FastAPI server with fetch method

--- main.py
+++ main.py
@@ -1,118 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-from amazoncaptcha import AmazonCaptcha
+from fastapi import FastAPI, HTTPException
+from web_scraping import amazon_scraping, flipkart_scraping
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+app = FastAPI()
 
-def amazon_scraping(search_query):
-    driver.get("https://www.amazon.in/")
-
+@app.post("/fetchData/")
+async def fetchData(user_input : str):
     try:
-        captcha_link = driver.find_element(By.TAG_NAME, "img").get_attribute("src")
-        captcha = AmazonCaptcha.fromlink(captcha_link)
-        captcha_value = AmazonCaptcha.solve(captcha)
+        amazon_list = amazon_scraping(user_input)
+        flipkart_list = flipkart_scraping(user_input)
+        return {
+            "amazon": amazon_list,
+            "flipkart": flipkart_list
+        }
 
-        captcha_text_field = driver.find_element(By.ID, "captchacharacters")
-        captcha_text_field.send_keys(captcha_value)
-
-        button = driver.find_element(By.TAG_NAME, "button")
-        button.click()
-    except:
-        pass
-
-    text_field = driver.find_element(By.ID, "twotabsearchtextbox")
-    text_field.send_keys(search_query + Keys.ENTER)
-
-    products = driver.find_elements(By.CSS_SELECTOR, "div[role='listitem']")
-
-    i = 0
-    products_shown = 0
-    while products_shown < 5:
-
-        product_image = products[i].find_element(By.CSS_SELECTOR, "img.s-image").get_attribute("src")
-
-        product_name = products[i].find_element(By.CSS_SELECTOR, "div[role='listitem'] h2.a-color-base").get_attribute("aria-label")
-
-        try:
-            product_price = products[i].find_element(By.CSS_SELECTOR, "span.a-price-whole").text
-        except:
-            i += 1
-            continue
-
-        product_link = products[i].find_element(By.CSS_SELECTOR, "a.a-link-normal").get_attribute("href")
-
-        if product_name.find("Sponsored Ad") == 0:
-            i += 1
-            continue
-
-        print("Product Image:", product_image)
-        print("Product Title:", product_name)
-        print("Product Price:", product_price)
-        print("Product Link:", product_link)
-        print("")
-
-        i += 1
-        products_shown += 1
-
-
-def flipkart_scraping(search_query : str):
-    driver.get("https://www.flipkart.in/")
-
-    text_field = driver.find_element(By.CSS_SELECTOR, "input[title = 'Search for Products, Brands and More']")
-    text_field.send_keys(search_query + Keys.ENTER)
-
-    is_grid = True
-    products = driver.find_elements(By.CLASS_NAME, "slAVV4")
-
-    if len(products) == 0:
-        is_grid = False
-        products = driver.find_elements(By.CLASS_NAME, "tUxRFH")
-
-    i = 0
-    products_shown = 0
-
-    while products_shown < 5:
-
-        try:
-            products[i].find_element(By.CSS_SELECTOR, "div.f8qK5m")
-            i += 1
-            continue
-        except:
-            pass
-
-        if is_grid:
-            product_image = products[i].find_element(By.CSS_SELECTOR, "img.DByuf4").get_attribute("src")
-            product_name = products[i].find_element(By.CSS_SELECTOR, "a.wjcEIp").get_attribute("title")
-            product_price = products[i].find_element(By.CSS_SELECTOR, "div.Nx9bqj").text
-            product_link = products[i].find_element(By.CSS_SELECTOR, "a.VJA3rP").get_attribute("href")
-        else:
-            product_image = products[i].find_element(By.CSS_SELECTOR, "img.DByuf4").get_attribute("src")
-            product_name = products[i].find_element(By.CSS_SELECTOR, "div.KzDlHZ").text
-            product_price = products[i].find_element(By.CSS_SELECTOR, "div.Nx9bqj").text
-            product_link = products[i].find_element(By.CSS_SELECTOR, "a.CGtC98").get_attribute("href")
-
-        print("Product Image:", product_image)
-        print("Product Name:", product_name)
-        print("Product Price:", product_price)
-        print("Product Link:", product_link)
-        print("")
-
-        i += 1
-        products_shown += 1
-
-
-
-
-print("Welcome to Price Tracker")
-search_query = input("Enter product name: ").strip().lower()
-
-amazon_scraping(search_query)
-flipkart_scraping(search_query)
-
-time.sleep(60)
-
-driver.quit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ web_scraping.py
@@ -0,0 +1,125 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from amazoncaptcha import AmazonCaptcha
+
+
+def amazon_scraping(search_query):
+    
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
+
+    driver.get("https://www.amazon.in/")
+
+    amazon_list = []
+
+    try:
+        captcha_link = driver.find_element(By.TAG_NAME, "img").get_attribute("src")
+        captcha = AmazonCaptcha.fromlink(captcha_link)
+        captcha_value = AmazonCaptcha.solve(captcha)
+
+        captcha_text_field = driver.find_element(By.ID, "captchacharacters")
+        captcha_text_field.send_keys(captcha_value)
+
+        button = driver.find_element(By.TAG_NAME, "button")
+        button.click()
+    except:
+        pass
+
+    text_field = driver.find_element(By.ID, "twotabsearchtextbox")
+    text_field.send_keys(search_query + Keys.ENTER)
+
+    products = driver.find_elements(By.CSS_SELECTOR, "div[role='listitem']")
+
+    i = 0
+    products_shown = 0
+    while products_shown < 5:
+
+        product_image = products[i].find_element(By.CSS_SELECTOR, "img.s-image").get_attribute("src")
+
+        product_name = products[i].find_element(By.CSS_SELECTOR, "div[role='listitem'] h2.a-color-base").get_attribute("aria-label")
+
+        try:
+            product_price = products[i].find_element(By.CSS_SELECTOR, "span.a-price-whole").text
+        except:
+            i += 1
+            continue
+
+        product_link = products[i].find_element(By.CSS_SELECTOR, "a.a-link-normal").get_attribute("href")
+
+        if product_name.find("Sponsored Ad") == 0:
+            i += 1
+            continue
+
+        product_info = {
+            "image": product_image,
+            "title": product_name,
+            "price": product_price,
+            "link": product_link
+        }
+
+        amazon_list.append(product_info)
+
+        i += 1
+        products_shown += 1
+
+    driver.quit()
+    return amazon_list
+
+
+def flipkart_scraping(search_query : str):
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
+
+    driver.get("https://www.flipkart.in/")
+
+    flipkart_list = []
+
+    text_field = driver.find_element(By.CSS_SELECTOR, "input[title = 'Search for Products, Brands and More']")
+    text_field.send_keys(search_query + Keys.ENTER)
+
+    is_grid = True
+    products = driver.find_elements(By.CLASS_NAME, "slAVV4")
+
+    if len(products) == 0:
+        is_grid = False
+        products = driver.find_elements(By.CLASS_NAME, "tUxRFH")
+
+    i = 0
+    products_shown = 0
+
+    while products_shown < 5:
+
+        try:
+            products[i].find_element(By.CSS_SELECTOR, "div.f8qK5m")
+            i += 1
+            continue
+        except:
+            pass
+
+        if is_grid:
+            product_image = products[i].find_element(By.CSS_SELECTOR, "img.DByuf4").get_attribute("src")
+            product_name = products[i].find_element(By.CSS_SELECTOR, "a.wjcEIp").get_attribute("title")
+            product_price = products[i].find_element(By.CSS_SELECTOR, "div.Nx9bqj").text
+            product_link = products[i].find_element(By.CSS_SELECTOR, "a.VJA3rP").get_attribute("href")
+        else:
+            product_image = products[i].find_element(By.CSS_SELECTOR, "img.DByuf4").get_attribute("src")
+            product_name = products[i].find_element(By.CSS_SELECTOR, "div.KzDlHZ").text
+            product_price = products[i].find_element(By.CSS_SELECTOR, "div.Nx9bqj").text
+            product_link = products[i].find_element(By.CSS_SELECTOR, "a.CGtC98").get_attribute("href")
+
+        product_info = {
+            "image": product_image,
+            "title": product_name,
+            "price": product_price,
+            "link": product_link
+        }
+
+        flipkart_list.append(product_info)
+
+        i += 1
+        products_shown += 1
+
+    driver.quit()
+    return flipkart_list
