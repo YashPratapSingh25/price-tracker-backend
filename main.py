e2700987050d38058f14e294d903cbe870b5cc96
YashPratapSingh25 <yash64572@gmail.com>
2025-03-03 14:15:03 +0530

    Add fetch deals method for home page

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from web_scraping import amazon_scraping, flipkart_scraping
+from web_scraping import amazon_scraping, flipkart_scraping, deals_scraping
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8000"],  # Specify allowed origins
+    allow_origins=["*"],  # Specify allowed origins
     allow_credentials=True,
     allow_methods=["GET", "POST"],  # Only allow required methods
     allow_headers=["*"],  # Allow all headers
@@ -16,15 +16,29 @@ app.add_middleware(
 class UserInput(BaseModel):
     query : str
 
+@app.get("/fetchDeals")
+async def fetchDeals():
+    try:
+        deals = deals_scraping()
+        json = {
+            "deals": deals
+        }
+        return json
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
 @app.post("/fetchData/")
 async def fetchData(user_input : UserInput):
     try:
         amazon_list = amazon_scraping(user_input.query)
         flipkart_list = flipkart_scraping(user_input.query)
-        return {
+        json = {
             "amazon": amazon_list,
             "flipkart": flipkart_list
         }
+        return json
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- web_scraping.py
+++ web_scraping.py
@@ -2,13 +2,58 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from amazoncaptcha import AmazonCaptcha
 
+from amazoncaptcha import AmazonCaptcha
+import time
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+def amazon_captcha_solver(src : str, driver):
+    captcha = AmazonCaptcha.fromlink(src)
+    captcha_value = AmazonCaptcha.solve(captcha)
+
+    captcha_text_field = driver.find_element(By.ID, "captchacharacters")
+    captcha_text_field.send_keys(captcha_value)
+
+    button = driver.find_element(By.TAG_NAME, "button")
+    button.click()
+
+
+def deals_scraping():
+
+    deals = []
+
+    
+    driver.get("https://www.amazon.in/b?node=30631803031&ref_=a2i_ohl_gw_cml_cta")
+
+    try:
+        captcha_link = driver.find_element(By.TAG_NAME, "img").get_attribute("src")
+        amazon_captcha_solver(src=captcha_link, driver=driver)
+    except:
+        pass
+    
+    products = driver.find_elements(By.CSS_SELECTOR, "div._octopus-search-result-card_style_apbSearchResultItem__2-mx4")
+
+    if len(products) == 0:
+        driver.refresh()
+        products = driver.find_elements(By.CSS_SELECTOR, "div._octopus-search-result-card_style_apbSearchResultItem__2-mx4")
+
+    for product in products:
+
+        info = {
+            "image": product.find_element(By.TAG_NAME, "img").get_attribute("src"),
+            "title" : product.find_element(By.CSS_SELECTOR, "h2 > span").text,
+            "current_price" : product.find_element(By.CSS_SELECTOR, "span.a-price-whole").text,
+            "previous_price" : product.find_element(By.CSS_SELECTOR, "span[data-a-strike='true']").text.replace("₹", ""),
+            "link" : product.find_element(By.TAG_NAME, "a").get_attribute("href")
+        }
+
+        deals.append(info)
+
+    return deals
 
 def amazon_scraping(search_query):
-    
-    service = Service(executable_path="chromedriver.exe")
-    driver = webdriver.Chrome(service=service)
 
     driver.get("https://www.amazon.in/")
 
@@ -16,14 +61,7 @@ def amazon_scraping(search_query):
 
     try:
         captcha_link = driver.find_element(By.TAG_NAME, "img").get_attribute("src")
-        captcha = AmazonCaptcha.fromlink(captcha_link)
-        captcha_value = AmazonCaptcha.solve(captcha)
-
-        captcha_text_field = driver.find_element(By.ID, "captchacharacters")
-        captcha_text_field.send_keys(captcha_value)
-
-        button = driver.find_element(By.TAG_NAME, "button")
-        button.click()
+        amazon_captcha_solver(src=captcha_link, driver=driver)
     except:
         pass
 
@@ -56,7 +94,8 @@ def amazon_scraping(search_query):
             "image": product_image,
             "title": product_name,
             "price": product_price,
-            "link": product_link
+            "link": product_link,
+            "site": "Amazon"
         }
 
         amazon_list.append(product_info)
@@ -64,23 +103,32 @@ def amazon_scraping(search_query):
         i += 1
         products_shown += 1
 
-    driver.quit()
     return amazon_list
 
 
 def flipkart_scraping(search_query):
 
-    service = Service(executable_path="chromedriver.exe")
-    driver = webdriver.Chrome(service=service)
-
     driver.get("https://www.flipkart.in/")
 
-    text_field = driver.find_element(By.CSS_SELECTOR, "input[title = 'Search for Products, Brands and More']")
-    text_field.send_keys(search_query + Keys.ENTER)
+    try:
+        text_field = driver.find_element(By.CSS_SELECTOR, "input[title = 'Search for Products, Brands and More']")
+        text_field.send_keys(search_query + Keys.ENTER)
+    except:
+        driver.refresh()
+        text_field = driver.find_element(By.CSS_SELECTOR, "input[title = 'Search for Products, Brands and More']")
+        text_field.send_keys(search_query + Keys.ENTER)
 
     flipkart_list = []
 
     is_clothing = False
+
+
+    try:
+        time.sleep(5)
+        button = driver.find_element(By.TAG_NAME, "button")
+        button.click()
+    except:
+        pass
 
     is_grid = True
     products = driver.find_elements(By.CLASS_NAME, "slAVV4")
@@ -129,16 +177,12 @@ def flipkart_scraping(search_query):
             product_price = products[i].find_element(By.CSS_SELECTOR, "div.Nx9bqj").text
             product_link = products[i].find_element(By.CSS_SELECTOR, "a.CGtC98").get_attribute("href")
 
-        print("Product Image:", product_image)
-        print("Product Name:", product_name)
-        print("Product Price:", product_price)
-        print("Product Link:", product_link)
-
         product_info = {
             "image": product_image,
             "title": product_name,
-            "price": product_price,
-            "link": product_link
+            "price": product_price.removeprefix("₹"),
+            "link": product_link,
+            "site": "Flipkart"
         }
 
         flipkart_list.append(product_info)
@@ -146,5 +190,4 @@ def flipkart_scraping(search_query):
         i += 1
         products_shown += 1
 
-    driver.quit()
     return flipkart_list
